baa18eee38f1fc0c6d9a680d0d1f6a23c1969f18
Sadhana <gangadharan.sadhana@gmail.com>
2024-06-11 09:50:16 -0500

    Initial Commit

---
+++ WeatherMorph/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  WeatherMorph
+//
+//  Created by Scholar on 6/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ WeatherMorph/WeatherMorphApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMorphApp.swift
+//  WeatherMorph
+//
+//  Created by Scholar on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMorphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
